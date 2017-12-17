767417a8417a4bc475876e7e69b274cb88ad0f30
guyverckw <33108899+guyverckw@users.noreply.github.com>
2017-12-18 00:13:46 +0800

    Notification module using OLED
    This is a new module that make use of OLED display.  Currently tested on 12864 and 12832 OLED displays.

---
+++ Steem_notify_oled.py
@@ -0,0 +1,81 @@
+from piston.steem import Steem
+from piston.account import Account
+from piston.blog import Blog
+from piston.post import Post
+
+from demo_opts import get_device
+from luma.core.virtual import terminal
+from PIL import ImageFont
+
+import os
+import threading
+import random
+import csv
+import urllib.request
+import time
+import RPi.GPIO as GPIO
+import sys
+
+# initialize Steem
+steem = Steem()
+
+# Prepare LED indicator
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.OUT, initial=GPIO.LOW)
+
+def make_font(name, size):
+	font_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'fonts', name))
+	return ImageFont.truetype(font_path, size)
+
+
+def main():
+# Set user account, no. of history retrieve everytime, transaction ID buffer array of 10 
+	account_name = 'guyverckw'
+	account = Account(account_name)
+	first = 9999999999
+	limit = 5
+	History = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+	History_ID = 1.0
+	message = ' '
+
+
+# Prepare for OLED display
+	font = make_font('ProggyTiny.ttf', 16)
+	term = terminal(device, font)
+	term.clear()
+	term.animate = False
+
+# keep checking transactions
+	while True:
+		index = 0
+
+# Get last 5 history, put ID into buffer array
+		for his in account.rawhistory(first, limit):
+			if History_ID == his[0]:
+				break
+			if his[1]['op'][0] == 'comment':
+				message = 'There is a comment from @' + his[1]['op'][1]['author']
+			elif his[1]['op'][0] == 'vote':
+				if his[1]['op'][1]['author'] == account_name:
+					message = 'There is a vote from @' + his[1]['op'][1]['voter'] + ' of ' + str(his[1]['op'][1]['weight']/100) +'%'
+			else: 
+				History[index] = his[0]
+				index += 1
+				continue
+			History[index] = his[0]
+			index += 1
+			print (message)
+			GPIO.output(11, GPIO.HIGH)
+			term.clear()
+			term.println(message)
+			time.sleep(1)
+			GPIO.output(11, GPIO.LOW)
+
+		History_ID = History[0] 
+
+if __name__ == '__main__':
+	try:
+		device = get_device()
+		main()
+	except KeyboardInterrupt:
+		pass
